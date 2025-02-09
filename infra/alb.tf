78801c1e84cda0d0b6cc372b4b6fa8538c1ca7be
Donatus Dee <getdzidon@gmail.com>
2025-02-09 01:44:47 +0100

    added EC2 instance attachment to alb target group

--- infra/alb.tf
+++ infra/alb.tf
@@ -27,6 +27,13 @@ resource "aws_lb_target_group" "tg" {
   }
 }
 
+# Attach EC2 Instance to Target Group
+resource "aws_lb_target_group_attachment" "tg_attachment" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.ubuntu.id
+  port             = 80
+}
+
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.E-comApp_alb.arn
   port              = 80
