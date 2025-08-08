3c21b104e8d74ef1c983b27e79f05d3b166e4aaf
Donatus Dee <getdzidon@gmail.com>
2025-08-08 23:17:53 +0200

    made changhes to s3 backend confifuration for tfstate file and infra-deploy.yml file and introduced oidc

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,6 +1,7 @@
 # AWS Provider Configuration
 
-# Note: the backend values here are hardcodded because the backend "(terraform { backend "s3" { ... } })" is initialized before any variables or data sources are evaluated. Terraform needs to know where to find your state file before it can evaluate anything else.
+# Note: the backend values here are hardcodded because the backend "(terraform { backend "s3" { ... } })" is initialized before any variables or data sources are evaluated. 
+# Terraform needs to know where to find your state file before it can evaluate anything else.
 terraform {
   required_providers {
     aws = {
