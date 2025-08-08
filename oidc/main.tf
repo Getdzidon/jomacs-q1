1e5ca44dedd7e4b14ea47700408711d220755775
Donatus Dee <getdzidon@gmail.com>
2025-08-09 01:50:02 +0200

    testing again

--- oidc/main.tf
+++ oidc/main.tf
@@ -9,16 +9,11 @@
 resource "aws_iam_openid_connect_provider" "github_actions_oidc" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  # The thumbprint_list must be up-to-date. These are the current values for GitHub.
-  # The first thumbprint is the primary; the second is a backup.
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
 }
 
-
-# The IAM role that GitHub Actions will assume.
-# The `assume_role_policy` is the most important part, defining who can assume the role.
-resource "aws_iam_role" "github_actions_api_access_role" {
-  name = "GithubActions"
+resource "aws_iam_role" "GithubActionsRole" {
+  name = "GithubActionsRole"
 
   # The trust policy for the role.
   assume_role_policy = jsonencode({
@@ -27,19 +22,13 @@ resource "aws_iam_role" "github_actions_api_access_role" {
       {
         "Effect" : "Allow",
         "Principal" : {
-          # This specifies the OIDC provider we just created.
           "Federated" : aws_iam_openid_connect_provider.github_actions_oidc.arn
         },
-        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Action" : "sts:AssumeRole",
         "Condition" : {
-          # The `aud` (audience) claim must be sts.amazonaws.com for the token
-          # to be considered valid for assuming an AWS role.
           "StringEquals" : {
             "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
           },
-          # The `sub` (subject) claim identifies the repository and branch.
-          # We use StringLike here to allow any branch to assume the role,
-          # as long as it's from your specified repository.
           "StringLike" : {
             "token.actions.githubusercontent.com:sub": "repo:getdzidon/jomacs-q1:*"
           }
@@ -56,7 +45,7 @@ resource "aws_iam_role" "github_actions_api_access_role" {
 # but should be restricted in a production environment.
 resource "aws_iam_role_policy" "github_actions_access_policy" {
   name = "github-actions-access-policy"
-  role = aws_iam_role.github_actions_api_access_role.id
+  role = aws_iam_role.GithubActionsRole.id
   policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
